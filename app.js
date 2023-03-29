3a5c427eb6e7a30509df92c871d44415f10bd3e2
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-03-29 22:15:54 +0200

    Adding CSS, more EJS and code refactoring

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + '/date.js');
 
 const app = express();
 
-let items = ['Buy Food','Cook Food','Eat Food'];
+const items = ['Buy Food', 'Cook Food', 'Eat Food'];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-    let today = new Date();
-    let options = {
-        weekday: 'long',
-        day: 'numeric',
-        month: 'long'
-    };
-
-    let day = today.toLocaleDateString('en-US', options)
-
-    res.render('list', { kindOfDay: day, newListItems: items });
+    const day = date.getDate();
+    res.render('list', { listTitle: day, newListItems: items });
 });
 
 app.post('/', function (req, res) {
-    let item = req.body.newItem;
-    items.push(item);
-    res.redirect('/');
+    const item = req.body.newItem;
+    if (req.body.list === 'Work') {
+        workItems.push(item);
+        res.redirect('/work');
 
-})
+    } else {
+        items.push(item);
+        res.redirect('/');
+    }
+});
+
+app.get('/work', function (req, res) {
+    res.render('list', { listTitle: 'Work List', newListItems: workItems })
+});
 
 app.listen(3000, function () {
     console.log('Server is running on port 3000');
